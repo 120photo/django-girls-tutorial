7a311c64fa4916de843a152156341d3c10699e5d
Jose Juarez <jose@juarez.me>
2017-01-26 21:13:25 -0500

    changed some settings and created "Blog" app

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ttu#xbhtu!g_(a((q742l95gp@!*!r6*5fxxqti*frh*98)z%v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pythonanywhere.com']
 
 
 # Application definition
